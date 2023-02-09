582ccffcc543188b6bbc5fabdbd61c58a2e492ce
Mohamed Farah <mohamedfarah0508@gmail.com>
2023-02-08 18:32:14 -0800

    playing with api book thumnail links

--- testAPI.js
+++ testAPI.js
@@ -1,11 +1,23 @@
 async function getBooks(query) {
   const apiKey = "AIzaSyD78Ix8MA22TZKPmBeVZpTmWtIzQPcPxwY";
-  const endpoint = `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}`;
+  const maxResults = 40;
+  const endpoint = `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=${maxResults}&key=${apiKey}`;
   const response = await fetch(endpoint);
   const data = await response.json();
 
-  for (const item of data.items) {
+  for (let item of data.items) {
     console.log(item.volumeInfo.title);
+  }
+  for (let item of data.items) {
+    console.log(item.volumeInfo.title);
+    if (
+      item.volumeInfo.hasOwnProperty("imageLinks") &&
+      item.volumeInfo.imageLinks.hasOwnProperty("thumbnail")
+    ) {
+      console.log(item.volumeInfo.imageLinks.thumbnail);
+    } else {
+      console.log("No Thumbnail Available");
+    }
   }
 }
 
