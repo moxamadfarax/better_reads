96479c7925b0ee3f4d62c6acbf7adbfed5dd2c34
Wir3s <aandrebess@gmail.com>
2023-02-09 21:34:09 -0600

    homepage route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,36 +3,57 @@ const { Books, Users } = require("../models");
 
 router.get("/", async (req, res) => {
   // Get all books and JOIN with user data
- try {
-  // const booksData = await Books.findAll({
-  //   include: [
-  //     {
-  //       model: Users,
-  //       attributes: ["name"],
-  //     },
-  //   ],
-  // });
+  try {
+    // const booksData = await Books.findAll({
+    //   include: [
+    //     {
+    //       model: Users,
+    //       attributes: ["name"],
+    //     },
+    //   ],
+    // });
 
-  // // Serialize data so the template can read it
-  // const books = booksData.map((project) => project.get({ plain: true }));
+    // // Serialize data so the template can read it
+    // const books = booksData.map((project) => project.get({ plain: true }));
 
-//  Pass serialized data and session flag into template
-  res.render("login");
-
-} catch (err) {
-  res.status(500).json(err);
-}
-});
-
-router.get("/login", (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect("/");
-    return;
+    //  Pass serialized data and session flag into template
+    res.render("login");
+  } catch (err) {
+    res.status(500).json(err);
   }
-
-  res.render("login");
 });
+
+router.get("/homepage", async (req, res) => {
+  // Get all books and JOIN with user data
+  try {
+    // const booksData = await Books.findAll({
+    //   include: [
+    //     {
+    //       model: Users,
+    //       attributes: ["name"],
+    //     },
+    //   ],
+    // });
+
+    // // Serialize data so the template can read it
+    // const books = booksData.map((project) => project.get({ plain: true }));
+
+    //  Pass serialized data and session flag into template
+    res.render("homepage");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// router.get("/login", (req, res) => {
+//   // If the user is already logged in, redirect the request to another route
+//   if (req.session.logged_in) {
+//     res.redirect("/");
+//     return;
+//   }
+
+//   res.render("login");
+// });
 
 router.get("users/:id", async (req, res) => {
   try {
