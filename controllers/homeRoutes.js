a0f609a9e21653dba8e8a83efb35a356de97979a
Your Name <berglundgibson@gmail.com>
2023-02-13 17:34:19 -0300

    fixed logged-in issue with homepage, added searchbar to homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,7 +7,9 @@ router.get("/", withAuth, async (req, res) => {
  
   try {
 
-    res.render("homepage");
+    res.render('homepage', {  
+      logged_in: req.session.logged_in 
+    });
   } catch (err) {
     res.status(500).json(err);
   }
