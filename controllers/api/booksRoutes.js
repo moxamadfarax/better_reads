4e96072804321a300c62eb205770c27fbe570cd5
Wir3s <aandrebess@gmail.com>
2023-02-13 19:30:48 -0600

    POST route for adding a book working

--- controllers/api/booksRoutes.js
+++ controllers/api/booksRoutes.js
@@ -2,12 +2,9 @@ const router = require("express").Router();
 const { Books } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-router.post("/", withAuth, async (req, res) => {
+router.post("/", async (req, res) => {
   try {
-    const newBook = await Books.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
+    const newBook = await Books.create(req.body);
 
     res.status(200).json(newBook);
   } catch (err) {
