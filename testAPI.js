f93190cb80cdc723139dd4b62199f55a84f8c1e0
Mohamed Farah <mohamedfarah0508@gmail.com>
2023-02-14 05:07:33 -0800

    fixing card gen

--- testAPI.js
+++ testAPI.js
@@ -1,47 +1,85 @@
 var btn = document.getElementById("submitButton");
+var inputField = document.getElementById("inputField");
+var nextBtn = document.getElementById("nextBtn");
+var previousBtn = document.getElementById("previousBtn");
 
-async function getData(query) {
+var startIndex = 0;
+var totalItems = 0;
+var itemsPerPage = 35;
+
+async function getData(query, startIndex) {
   const apiKey = "AIzaSyD78Ix8MA22TZKPmBeVZpTmWtIzQPcPxwY";
-  const maxResults = 40;
-  const endpoint = `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=${maxResults}&key=${apiKey}`;
+  const maxResults = itemsPerPage;
+  const endpoint = `https://www.googleapis.com/books/v1/volumes?q=${query}&startIndex=${startIndex}&maxResults=${maxResults}&key=${apiKey}`;
   const response = await fetch(endpoint);
   const data = await response.json();
 
-  for (let item of data.items) {
-    if (
-      item.volumeInfo.hasOwnProperty("imageLinks") &&
-      item.volumeInfo.imageLinks.hasOwnProperty("thumbnail")
-    ) {
-      console.log(item.volumeInfo.imageLinks.thumbnail);
-    } else {
-      console.log("No Thumbnail Available");
+  if (data.items) {
+    const resultsContainer = document.getElementById("resultsContainer");
+    resultsContainer.innerHTML = "";
+
+    for (let item of data.items) {
+      if (
+        item.volumeInfo.hasOwnProperty("imageLinks") &&
+        item.volumeInfo.imageLinks.hasOwnProperty("thumbnail")
+      ) {
+        const card = generateBookCard(item.volumeInfo);
+
+        resultsContainer.appendChild(card);
+      }
     }
+  } else {
+    const resultsContainer = document.getElementById("resultsContainer");
+    resultsContainer.innerHTML = "<p>No Results Found</p>";
   }
-  const resultsContainer = document.getElementById("resultsContainer");
-  resultsContainer.innerHTML = "";
 
-  for (let item of data.items) {
-    if (
-      item.volumeInfo.hasOwnProperty("imageLinks") &&
-      item.volumeInfo.imageLinks.hasOwnProperty("thumbnail")
-    ) {
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
+  return data.totalItems;
+}
 
-      const coverImage = document.createElement("img");
-      coverImage.setAttribute("src", item.volumeInfo.imageLinks.thumbnail);
-      coverImage.setAttribute("alt", item.volumeInfo.title);
-      card.appendChild(coverImage);
+function generateBookCard(bookInfo) {
+  const card = document.createElement("div");
+  card.classList.add("card", "book-card");
 
-      resultsContainer.appendChild(card);
-    }
-  }
+  const coverImage = document.createElement("img");
+  coverImage.classList.add("card-img-top");
+  coverImage.src = bookInfo.imageLinks.thumbnail;
+  coverImage.alt = bookInfo.title;
+  card.appendChild(coverImage);
+
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+
+  const title = document.createElement("h5");
+  title.classList.add("card-title");
+  title.textContent = bookInfo.title;
+  cardBody.appendChild(title);
+
+  card.appendChild(cardBody);
+
+  return card;
 }
 
 btn.addEventListener("click", function () {
-  console.clear();
-  var inputValue = document.getElementById("inputField").value;
-  getData(inputValue).then(() => {
-    console.log("Finished retrieving book thumbnails");
+  var inputValue = inputField.value;
+  startIndex = 0;
+  getData(inputValue, startIndex).then(() => {
+    console.log("Finished retrieving book data");
   });
+});
+
+nextBtn.addEventListener("click", function () {
+  startIndex += itemsPerPage;
+  getData(inputField.value, startIndex);
+});
+
+previousBtn.addEventListener("click", function () {
+  startIndex -= itemsPerPage;
+  getData(inputField.value, startIndex);
+});
+
+inputField.addEventListener("keydown", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    btn.click();
+  }
 });
