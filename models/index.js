2f1dfe1728a194bddb976e016cc5fed985974e6c
Wir3s <aandrebess@gmail.com>
2023-02-13 18:39:03 -0600

    Added model for comments and fixed associations between users and books

--- models/index.js
+++ models/index.js
@@ -1,11 +1,22 @@
 const Books = require("./Books");
 const Users = require("./Users");
+const Comments = require("./Comments");
 
 Users.hasMany(Books, {
-  foreignKey: "books_id",
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 Books.hasOne(Users, {
   foreignKey: "user_id",
 });
 
-module.exports = { Users, Books };
+Books.hasMany(Comments, {
+  foreignKey: "books_id",
+  onDelete: "CASCADE",
+});
+
+Comments.hasOne(Books, {
+  foreignKey: "books_id",
+});
+
+module.exports = { Users, Books, Comments };
