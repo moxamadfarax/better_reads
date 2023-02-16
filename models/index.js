b33016e38caf2f45c5ee168ed972d531583d499f
Wir3s <aandrebess@gmail.com>
2023-02-15 19:25:51 -0600

    require lodash

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 const Books = require("./Books");
 const Users = require("./Users");
-const Comments = require("./Comments");
 
 Users.hasMany(Books, {
   foreignKey: "user_id",
@@ -10,13 +9,6 @@ Books.hasOne(Users, {
   foreignKey: "user_id",
 });
 
-Books.hasMany(Comments, {
-  foreignKey: "books_id",
-  onDelete: "CASCADE",
-});
 
-Comments.hasOne(Books, {
-  foreignKey: "books_id",
-});
 
-module.exports = { Users, Books, Comments };
+module.exports = { Users, Books };
