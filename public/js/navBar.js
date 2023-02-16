100233e5acbee570270d6cce6a6b840be06fb8c7
Mohamed Farah <mohamedfarah0508@gmail.com>
2023-02-16 13:13:38 -0800

    trying to add responsiveness

--- public/js/navBar.js
+++ public/js/navBar.js
@@ -1,7 +1,1 @@
-const hamburgerButton = document.querySelector(".navbar-toggler");
 
-const navbarCollapse = document.querySelector(".navbar-collapse");
-
-hamburgerButton.addEventListener("click", function () {
-  navbarCollapse.classList.toggle("collapse");
-});
