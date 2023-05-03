1884d35a036eac5e19a85afe3c4d89b2255f3d53
Mohamed Farah <mohamedfarah0508@gmail.com>
2023-05-02 18:22:20 -0700

    deleteting password protection

--- controllers/api/usersRoutes.js
+++ controllers/api/usersRoutes.js
@@ -88,7 +88,6 @@ router.post("/login", async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.user_id;
       req.session.logged_in = true;
-
       res.json({ user: userData, message: "You are now logged in!" });
     });
   } catch (err) {
